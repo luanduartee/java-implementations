c152a23a28eafefb72915d7f3d6c7bd4ce1d62cd
Luan Mateus <luanmateus.duarte@gmail.com>
2024-09-17 10:19:47 -0300

    :sparkles: feat: Adicionando classe Function

--- stream_api/src/functional_interface/SupplierExample.java
+++ stream_api/src/functional_interface/SupplierExample.java
@@ -7,10 +7,10 @@ import java.util.stream.Stream;
 public class SupplierExample {
 	  public static void main(String[] args) {
 	    // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
-    Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+
 
     // Usar o Supplier para obter uma lista com 5 saudações
-    List<String> listaSaudacoes = Stream.generate(saudacao)
+    List<String> listaSaudacoes = Stream.generate(() -> "Olá, seja bem-vindo(a)")
         .limit(5)
         .toList();
 
