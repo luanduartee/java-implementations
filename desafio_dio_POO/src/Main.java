f01735302eb3c81c1c58cc3853f53f1874fff425
luanduartee <luanmateus.duarte@gmail.com>
2024-10-03 13:28:25 -0300

    Adicionando classes de Mentoria e Curso

---
+++ desafio_dio_POO/src/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+
+        System.out.println(curso1);
+    }
+}
